55bf6d83ee13bbaf8ebb275ea83e7f29740823d1
Алексей Шевченко <shevchenko201511@gmail.com>
2021-05-13 02:42:56 +0300

    add flip logic

--- scripts/load-handler.js
+++ scripts/load-handler.js
@@ -1,9 +1,15 @@
-let startPage = 'content/introduction.html';
-let links = document.getElementById('navigation').getElementsByTagName('a');
-let contentBlock = document.getElementsByTagName('article')[0];
+let pages = getPages();
+let startPage = pages[0];
 let currentPage = startPage;
 
+let links = document.getElementById('navigation').getElementsByTagName('a');
+let contentBlock = document.getElementsByTagName('article')[0];
+
+let nextButton = document.getElementById("next");
+let prevButton = document.getElementById("prev");
+
 load(startPage);
+checkPagesBorder();
 
 for (let index = 0; index < links.length; index++) {
     links[index].onclick = function(e) {
@@ -17,9 +23,51 @@ for (let index = 0; index < links.length; index++) {
     };
 }
 
+nextButton.onclick = function() {
+    flipPage(1);
+    checkPagesBorder();
+}
+prevButton.onclick = function() {
+    flipPage(-1);
+    checkPagesBorder();
+}
+
+function flipPage(direction)
+{
+    let currentPageIndex = pages.indexOf(currentPage);
+    let newPage = pages[currentPageIndex + direction];
+
+    currentPage = newPage;
+    load(currentPage);
+}
+
+function checkPagesBorder()
+{
+    if (currentPage == pages[0])
+        prevButton.disabled = true;
+    else
+        prevButton.disabled = false;
+
+    if (currentPage == pages[pages.length - 1])
+        nextButton.disabled = true;
+    else
+        nextButton.disabled = false;
+}
+
 function load(link) {
     let request = new XMLHttpRequest();
         request.open('get', link);
         request.onload = () => contentBlock.innerHTML = request.response;
         request.send();
+}
+
+function getPages() {
+    let links = document.getElementById('navigation').getElementsByTagName('a');
+    let pages = [];
+
+    for (let i = 0; i < links.length; i++)
+        if (links[i].href.includes('content/'))
+            pages.push(links[i].href);
+
+    return pages;
 }
